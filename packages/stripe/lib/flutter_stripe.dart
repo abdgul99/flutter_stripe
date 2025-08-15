d83d60354b7e8c823a661dcf09c627f2510159f8
Abd Ullah <67420869+abdgul99@users.noreply.github.com>
2025-08-15 23:55:15 +0500

    jitsi merged

--- packages/stripe/lib/flutter_stripe.dart
+++ packages/stripe/lib/flutter_stripe.dart
@@ -1,5 +1,14 @@
 export 'package:stripe_platform_interface/stripe_platform_interface.dart';
 
+export 'src-jitsi/features_flag/feature_flag_video_resolutions.dart'
+    show FeatureFlagVideoResolutions;
+export 'src-jitsi/features_flag/feature_flags.dart' show FeatureFlags;
+// JitsiMeets
+export 'src-jitsi/jitsi_meet.dart' show JitsiMeet;
+export 'src-jitsi/jitsi_meet_conference_options.dart'
+    show JitsiMeetConferenceOptions;
+export 'src-jitsi/jitsi_meet_event_listener.dart' show JitsiMeetEventListener;
+export 'src-jitsi/jitsi_meet_user_info.dart' show JitsiMeetUserInfo;
 export 'src/model/apple_pay_button.dart';
 export 'src/stripe.dart';
 export 'src/widgets/adress_sheet.dart';
